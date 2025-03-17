1eff1d6bbe2c1ebf3304a136aa641f57d186dd04
frank collebrusco <collebrusco.frank@gmail.com>
2025-03-17 00:31:44 -0500

    all but royal maybe

--- poker.cpp
+++ poker.cpp
@@ -1,8 +1,18 @@
+#include <cstddef>
+#include <cstdint>
 #include <stdio.h>
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
 
+#include <random>
+
+static size_t rand_int(size_t a, size_t b) {
+    static std::random_device rd;  // Non-deterministic random source
+    static std::mt19937 gen(rd()); // Mersenne Twister PRNG seeded with rd()
+    std::uniform_int_distribution<size_t> dist(a, b);
+    return dist(gen);
+}
 #define lg printf
 static inline void nl() {lg("\n");}
 
@@ -50,6 +60,25 @@ typedef enum {
     SUIT_LAST,
 } suit_e;
 
+typedef enum {
+    SUITBMP_NONE = 0,
+    SUITBMP_HEARTS = 1,
+    SUITBMP_DIAMONDS = 2,
+    SUITBMP_SPADES = 4,
+    SUITBMP_CLUBS = 8,
+    SUITBMP_LAST = 16,
+} suit_bmp_e;
+
+static suit_bmp_e suit_to_bmp(suit_e suit) {
+    static const suit_bmp_e suits[] = {
+        SUITBMP_HEARTS,
+        SUITBMP_DIAMONDS,
+        SUITBMP_SPADES,
+        SUITBMP_CLUBS
+    };
+    return suits[suit];
+}
+
 static const char* suit_name(suit_e suit) {
     static const char* suit_names[] = {
         "HEARTS",
@@ -68,14 +97,35 @@ struct Card {
     }
 };
 
-#include <random>
+typedef enum {
+    HAND_HIGHCARD = 0,
+    HAND_PAIR,
+    HAND_2PAIR,
+    HAND_3OFAKIND,
+    HAND_STRAIGHT,
+    HAND_FLUSH,
+    HAND_FULLHOUSE,
+    HAND_4OFAKIND,
+    HAND_STRAIGHT_FLUSH,
+    HAND_ROYAL_FLUSH,
+} hand_e;
 
-static size_t rand_int(size_t a, size_t b) {
-    static std::random_device rd;  // Non-deterministic random source
-    static std::mt19937 gen(rd()); // Mersenne Twister PRNG seeded with rd()
-    std::uniform_int_distribution<size_t> dist(a, b);
-    return dist(gen);
+static const char* hand_name(hand_e hand) {
+    static const char* names[] = {
+        "HIGHCARD",
+        "PAIR",
+        "TWO PAIR",
+        "THREEOFAKIND",
+        "STRAIGHT",
+        "FLUSH",
+        "FULLHOUSE",
+        "FOUROFAKIND",
+        "STRAIGHT_FLUSH",
+        "ROYAL_FLUSH",
+    };
+    return names[hand];
 }
+
 
 struct Deck : private std::vector<Card> {
     Deck(bool empty = false) {
@@ -106,6 +156,99 @@ struct Deck : private std::vector<Card> {
         return hand;
     }
 
+    static hand_e better_hand(hand_e a, hand_e b) {
+        return a > b ? a : b;
+    }
+
+    hand_e find_best_hand() {
+        hand_e power = HAND_HIGHCARD;
+
+        
+
+        uint32_t suit_ctr[SUIT_LAST] = {0};
+        struct {
+            uint32_t count;
+            uint32_t suitmap;
+        } rank_ctr[RANK_LAST] = {0};
+
+        for (auto card : *this) {
+            suit_ctr[card.suit]++;
+            if (suit_ctr[card.suit] > 4) {
+                power = better_hand(power, HAND_FLUSH);
+            }
+            rank_ctr[card.rank].count++;
+            rank_ctr[card.rank].suitmap |= suit_to_bmp(card.suit);
+        }
+
+        bool pair = false;
+        bool trip = false;
+        size_t consec = 0;
+        size_t cursuit = ~0;
+        for (size_t r = 0; r < RANK_LAST; r++) {
+            switch (rank_ctr[r].count) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                power = better_hand(power, pair ? HAND_2PAIR : HAND_PAIR);
+                pair = true;
+                break;
+            case 3:
+                power = better_hand(power, HAND_3OFAKIND);
+                trip = true;
+                break;
+            case 4:
+                power = better_hand(power, HAND_4OFAKIND);
+                break;
+            default:
+                break;
+            }
+
+            if (rank_ctr[r].count) {
+                /* have > 0 of rank r */
+                if (consec == 0) {
+                    /* first of a potential consec string */
+                    cursuit = ~0;
+                }
+                consec++;
+                cursuit &= rank_ctr[r].suitmap;
+                if (consec > 4) {
+                    power = better_hand(power, cursuit ? HAND_STRAIGHT_FLUSH : HAND_STRAIGHT);
+                }
+            } else {
+                /* have none of rank r */
+                cursuit = ~0;
+                consec = 0;
+            }
+        }
+
+        if (pair && trip) {
+            power = better_hand(power, HAND_FULLHOUSE);
+        }
+
+        return power;
+    }
+
+    static Deck get_str8flush() {
+        Deck hand(true);
+        hand.push_back(Card{RANK_10, SUIT_HEARTS});
+        hand.push_back(Card{RANK_JACK, SUIT_HEARTS});
+        hand.push_back(Card{RANK_QUEEN, SUIT_HEARTS});
+        hand.push_back(Card{RANK_KING, SUIT_HEARTS});
+        hand.push_back(Card{RANK_ACE, SUIT_HEARTS});
+        return hand;
+    }
+    static Deck get_flush() {
+        Deck hand(true);
+        hand.push_back(Card{RANK_10, SUIT_HEARTS});
+        hand.push_back(Card{RANK_JACK, SUIT_HEARTS});
+        hand.push_back(Card{RANK_QUEEN, SUIT_CLUBS});
+        hand.push_back(Card{RANK_KING, SUIT_HEARTS});
+        hand.push_back(Card{RANK_ACE, SUIT_HEARTS});
+        return hand;
+    }
+
     void print() {
         for (auto card : *this) {
             card.print(); lg("\n");
@@ -129,10 +272,16 @@ int main() {
         auto hand = deck.deal(7);
 
         nl();
-
+        lg("hand:\n");
         hand.print();
+        lg("\n and the best hand there is %s\n", hand_name(hand.find_best_hand()));
 
     }
+
+    // Deck sf = Deck::get_flush();
+    // sf.print();
+
+    // lg("best: %s\n", hand_name(sf.find_best_hand()));
 
     return 0;
 }
